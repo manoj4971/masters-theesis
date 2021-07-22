ce3822a09d3347adba365c154168f3c31df7ee5a
manoj4971 <manojsai4971@gmail.com>
2021-07-22 12:13:23 +0200

    docker-hello world

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "i am manoj"
+    
+
+# if __name__ == "__main__":
+#     # Only for debugging while developing8
+#     app.run(host='0.0.0.0', debug=True, port=80)
